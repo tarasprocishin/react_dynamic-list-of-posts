b5e184ee4b8de35e1322f0432ea03090b145a5e8
tarasprocishin <taras1procishin@gmail.com>
2019-07-03 15:22:14 +0300

    Updates

---
+++ precache-manifest.09558e4fb689c8207a59da4b61607620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8053a4e16942d53f546fa35a95e49e60",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "e9b7bbe978b1e65cfbcc",
+    "url": "/react_dynamic-list-of-posts/static/css/main.005f1985.chunk.css"
+  },
+  {
+    "revision": "522c2e1b3f687a3c6fcb",
+    "url": "/react_dynamic-list-of-posts/static/js/2.3700d4c4.chunk.js"
+  },
+  {
+    "revision": "e9b7bbe978b1e65cfbcc",
+    "url": "/react_dynamic-list-of-posts/static/js/main.59e6fc97.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
